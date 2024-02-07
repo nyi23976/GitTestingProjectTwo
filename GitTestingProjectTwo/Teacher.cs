d15b153e80db67893edf2995f04b884aa11a0a6c
nyi23976 <nyi23976@gmail.com>
2024-02-07 23:38:01 +0630

    updateNNA

---
+++ GitTestingProjectTwo/Teacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestingProjectTwo
+{
+    internal class Teacher
+    {
+        public int age { get; set; }
+    }
+}
